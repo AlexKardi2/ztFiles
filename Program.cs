97ba9a62fb847650eafd4200483039697547125c
AlexKardi2 <alexkardi2@gmail.com>
2024-09-08 01:45:37 +0300

    some functionality added
    on program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleAppGit
         static void Main(string[] args)
         {
             SecondClass _second = new();
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(_second.GetType().ToString());
         }
     }
 }
